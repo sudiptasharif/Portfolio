3fc63a959d19c8d26cd2fabe20fe08f1bc5e41e2
Sudipta Sharif <sudiptasharif@gmail.com>
2022-02-09 02:55:46 -0600

    Finished implementing the toggle feature for work experiance

--- js/index.js
+++ js/index.js
@@ -4,8 +4,15 @@ var meDetailsEle = document.getElementById("me_details");
 var meDetailsExpand = false; 
 var meHeadingEleText = "+ About Me";
 
+var workDivEle = document.getElementById("work");
+var workHeadingEle = document.getElementById("work_heading");
+var workDetailsEle = document.getElementById("work_details");
+var workDetailsExpand = false;
+var workHeadingEleText = "+ Work Experiances"; 
+
 const main = function() {
     meHeadingEle.addEventListener("click", aboutMe);
+    workHeadingEle.addEventListener("click", workExperiances);
 }
 
 const aboutMe = function() {
@@ -20,6 +27,20 @@ const aboutMe = function() {
     }
     setVisible(meDetailsEle, meDetailsExpand);
     setText(meHeadingEle, meHeadingEleText);   
+}
+
+const workExperiances = function() {
+    if (workDetailsExpand) {
+        workDetailsExpand = false;
+        workHeadingEleText = "+ Work Experiances"; 
+        removeChildLast(workDivEle);
+    } else {
+        workDetailsExpand = true;
+        workHeadingEleText = "- Work Experiances"; 
+        appendChildLast(workDivEle, workDetailsEle);
+    }
+    setVisible(workDetailsEle, workDetailsExpand);
+    setText(workHeadingEle, workHeadingEleText);     
 }
 
 window.onload = function() {
